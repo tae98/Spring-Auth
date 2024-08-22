81167eada11d406c058c4f6df0a04e481952c264
taehyunkim <98rlaxogus@gmail.com>
2024-08-22 11:57:20 +0900

    LoginPage Api

---
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.sparta.springauth.controller;
+
+import com.sparta.springauth.DTO.SignupRequestDto;
+import com.sparta.springauth.Service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/user/login-page")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/user/signup")
+    public String signupPage() {
+        return "signup";
+    }
+
+    @PostMapping("/user/signup")
+    public String signUp(SignupRequestDto requestDto){
+        userService.signup(requestDto);
+        return "redirect:/api/user/login-page";
+    }
+}
